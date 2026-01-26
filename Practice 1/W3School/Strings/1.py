d73af8507d2d7f02845e6ba045fe8d5e9dac1dfe
satpaev <bs13122006@gmail.com>
2026-01-26 14:19:42 +0500

    Initial commit

---
+++ Practice 1/W3School/Strings/1.py
@@ -0,0 +1,30 @@
+print("Hello")
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+  print(x)
+
+a = "Hello, World!"
+print(len(a))
---
+++ Practice 1/W3School/Syntax/1.py
@@ -0,0 +1,16 @@
+if 5 > 2:
+    print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
+
+print("Python is fun!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")    
